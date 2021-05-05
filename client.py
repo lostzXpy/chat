7e48014244cdb10ccce8f6e6fcc09139e71608ff
LostzX <79033314+lostzXpy@users.noreply.github.com>
2021-05-04 22:53:11 -0300

    Update client.py

--- client.py
+++ client.py
@@ -29,7 +29,7 @@ def client_receive():
 
 def client_send():
     while True:
-        message = f'<\033[1;31m@{alias}\033[m> {input("")}'
+        message = f'<\033[1;31m@{alias}\033[m> {input("\033[1;31m>>> \033[m")}'
         client.send(message.encode('utf-8'))
 
 
