f1c7274c27337f60ffb3a966005110ffdd93dd1d
LostzX <79033314+lostzXpy@users.noreply.github.com>
2021-05-04 21:47:38 -0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,39 @@
+import threading
+import socket
+import os 
+
+def clear():
+    os.system('clear')
+    
+clear()
+
+a = '[\033[1;31mi\033[m]'
+alias = input('{} Digite seu nickname: \033[1;31m')
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 59000))
+
+def client_receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            if message == "alias?":
+                client.send(alias.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            print('\n\033[1;31mHouve um erro desconhecido\n')
+            client.close()
+            break
+
+
+def client_send():
+    while True:
+        message = f'{alias}: {input("")}'
+        client.send(message.encode('utf-8'))
+
+
+receive_thread = threading.Thread(target=client_receive)
+receive_thread.start()
+
+send_thread = threading.Thread(target=client_send)
+send_thread.start()
