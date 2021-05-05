e604a85f8274f243d8276939c48e652aeccceee7
LostzX <79033314+lostzXpy@users.noreply.github.com>
2021-05-04 23:15:35 -0300

    Update client.py

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ def clear():
 clear()
 
 a = '[\033[1;31mi\033[m]'
-alias = input('{} Digite seu nickname: \033[1;31m')
+alias = input('{} Digite seu nickname: \033[1;31m'.format(a))
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 59000))
 
