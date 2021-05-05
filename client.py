831e9e5ed211d5ca56568ebf3a7a849bc4735c74
LostzX <79033314+lostzXpy@users.noreply.github.com>
2021-05-04 23:19:01 -0300

    Update client.py

--- client.py
+++ client.py
@@ -9,14 +9,15 @@ clear()
 
 a = '[\033[1;31mi\033[m]'
 alias = input('{} Digite seu nickname: \033[1;31m'.format(a))
+clear()
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 59000))
+client.connect(('127.0.0.1', 59002))
 
 def client_receive():
     while True:
         try:
             message = client.recv(1024).decode('utf-8')
-            if message == "alias?":
+            if message == ">>> ":
                 client.send(alias.encode('utf-8'))
             else:
                 print(message)
@@ -28,7 +29,7 @@ def client_receive():
 
 def client_send():
     while True:
-        message = f'{alias}: {input("")}'
+        message = f'<\033[1;31m@{alias}\033[m> {input("")}'
         client.send(message.encode('utf-8'))
 
 
